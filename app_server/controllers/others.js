23c71ad821ca44ec7ccb0c79ef2b868ce1937189
Prateek Joshi <prateekjoshi2013@gmail.com>
2016-05-20 07:43:19 -0400

    add new routes

---
+++ app_server/controllers/others.js
@@ -0,0 +1,6 @@
+module.exports.about = function (req, res) {
+    res.render('index', {
+        title: 'About'
+    });
+
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-/* GET home page. */
-var homepageController = function (req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
-}
-router.get('/', ctrlMain.index);
+//var ctrlMain = require('../controllers/main');
+
+var ctrlLocations = require('../controllers/locations.js');
+var ctrlOthers = require('../controllers/others.js');
+
+///* GET home page. */
+//var homepageController = function (req, res, next) {
+//        res.render('index', {
+//            title: 'Express'
+//        });
+//    }
+//router.get('/', ctrlMain.index);
+
+//locations pages
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+//Other pages
+router.get('/about', ctrlOthers.about);
 
 
 module.exports = router;
